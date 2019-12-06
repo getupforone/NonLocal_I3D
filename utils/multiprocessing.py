2785d3f3f38a726cc0be23543ba669e9780a9962
Administrator <giilkwon@nfri.re.kr>
2019-12-06 13:01:54 +0900

    create the multiprocessing.py

---
+++ utils/multiprocessing.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+import torch
+
+def run(
+    local_rank, num_proc, func, init_method, shard_id, num_shards, backend, cfg
+):
+    world_size = num_proc * num_shards
+    rank = shard_id * num_proc + local_rank
+
+    try:
+        torch.distributed.init_process_group(
+            backend=backend,
+            init_method=init_method,
+            world_size=world_size,
+            rank=rank,
+        )
+    except Exception as e:
+        raise e
+    
+    torch.cuda.set_device(local_rank)
+    func(cfg)
