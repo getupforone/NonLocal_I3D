c34e3ee99c0c388423be8ec8edaad8fa056c7441
Administrator <giilkwon@nfri.re.kr>
2019-12-06 16:12:58 +0900

    fix the dataset path

--- data/davis/get_davis_dataset_dir_list.py
+++ data/davis/get_davis_dataset_dir_list.py
@@ -2,14 +2,15 @@ import os
 import numpy as np
 print("thank you")
 
-dirs = ['/Users/giilkwon/WorkSpace/datasets/DAVIS/JPEGImages/480p']
+#dirs = ['/Users/giilkwon/WorkSpace/datasets/DAVIS/JPEGImages/480p']
+dirs = ['/data3/DAVIS/JPEGImages/480p']
 
 file_write_obj = open('davis_seqs_list.txt','w')
 for dir in dirs:
     seqs = np.sort(os.listdir(dir))
-    for seq in seqs:
+    for ind, seq in enumerate(seqs):
         seq_path = os.path.join(dir,seq)
-        line = "{}  {}".format(seq_path, seq)
+        line = "{}  {}".format(seq_path, ind)
         print(line)
         file_write_obj.writelines(line)
         #file_write_obj.writelines(seq)
