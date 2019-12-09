85c759b6770fe77bf2bef697eddfbda052ba1c30
Administrator <giilkwon@nfri.re.kr>
2019-12-09 10:26:56 +0900

    test

--- datasets/davis.py
+++ datasets/davis.py
@@ -118,11 +118,11 @@ class Davis(torch.utils.data.Dataset):
             raise NotImplementedError(
                 "Does not support {} mode".format(self.mode)    
             )
-        try:
-            print("path_to_seq_imgs = {}".format(self._path_to_seq_imgs))
+        try:            
             seq_dir = self._path_to_seq_imgs[index].strip()
             path_to_frames = np.sort(os.listdir(seq_dir))
             path_to_frames_length = len(path_to_frames)
+            print("!!! length={}".path_to_frames_length)
         except Exception as e:
             logger.info(
                 "Failed to load video from {} with error {}".format(
